42416758fb85a8caafba7f2b1af3d300246e92a6
Chris Roberts <chrisroberts.code@gmail.com>
2012-10-09 11:59:17 -0700

    Provide optional installation via source or package

--- attributes/default.rb
+++ attributes/default.rb
@@ -1,2 +1,7 @@
+default['tmux']['install_via'] = if(node['platform_family'] == 'rhel')
+  'source'
+else
+  'package'
+end
 default['tmux']['version'] = '1.6'
 default['tmxu']['checksum'] = '8756f6bcecb18102b87e5d6f5952ba2541f68ed3'
